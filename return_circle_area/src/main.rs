eff3eb9e84f5c48eed00cb092798fc439dfcbeb5
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-02 18:35:11 +0900

    return_circle_area associated function

--- return_circle_area/src/main.rs
+++ return_circle_area/src/main.rs
@@ -5,20 +5,16 @@ struct Circle {
 }
 
 impl Circle {
-    fn area(&self) -> f64 {
-        std::f64::consts::PI * (self.radius * self.radius)
-    }
-
-    fn grow(&self, increment: f64) -> Circle {
-        Circle { x: self.x, y: self.y, radius: self.radius + increment }
+    fn new(x: f64, y: f64, radius: f64) -> Circle {
+        Circle {
+            x: x,
+            y: y,
+            radius: radius,
+        }
     }
 }
 
 
 fn main() {
-    let c = Circle { x: 0.0, y: 0.0, radius: 2.0 };
-    println!("{}", c.area());
-
-    let d = c.grow(2.0).area();
-    println!("{}", d);
+    let c = Circle::new(0.0, 0.0, 2.0);
 }
