e988d7332cad3705a722a463d834daeb469df9a6
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-02 18:25:35 +0900

    return_circle_area implement area

---
+++ return_circle_area/target/debug/libreturn_circle_area.d
@@ -0,0 +1,1 @@
+/Users/kazukihigashiguchi/src/hobby/rust/snippets/return_circle_area/target/debug/libreturn_circle_area.rlib: /Users/kazukihigashiguchi/src/hobby/rust/snippets/return_circle_area/src/lib.rs
