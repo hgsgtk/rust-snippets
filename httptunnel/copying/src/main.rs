7540d500a40a168e2734f2f3cd2498d4d1b9cbad
hgsgtk <hgsgtk@gmail.com>
2021-08-27 21:27:57 +0900

    feat: add log4rs logging

--- httptunnel/copying/src/main.rs
+++ httptunnel/copying/src/main.rs
@@ -4,8 +4,28 @@ use tokio::io;
 
 #[tokio::main]
 pub async fn main() -> io::Result<()> {
-    println!("Hello, world!");
+    init_logger();
+
     // Contains the success value
     // https://doc.rust-lang.org/std/result/enum.Result.html#variant.Ok
     Ok(())
+}
+
+fn init_logger() {
+    // let: bind a value to variable 
+    // https://doc.rust-lang.org/std/keyword.let.html
+    let logger_configuration = "./config/log4rs.yaml";
+    //  Err(E) Contains the error value
+    // https://doc.rust-lang.org/std/result/enum.Result.html#variant.Err
+    // log4rs: log4rs is a highly configurable logging framework modeled after Java's Logback and log4j libraries.
+    // https://docs.rs/log4rs/1.0.0/log4rs
+    // Trait std::default::Default
+    // https://doc.rust-lang.org/std/default/trait.Default.html
+    if let Err(e) = log4rs::init_file(logger_configuration, Default::default()) {
+        // https://doc.rust-lang.org/std/macro.println.html
+        println!(
+            "Cannot initialize logger from {}, error=[{}]. Logging to the console.",
+            logger_configuration, e
+        );
+    }
 }
