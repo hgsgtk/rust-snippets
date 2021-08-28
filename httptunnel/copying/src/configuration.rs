cc49d88a07357ddb33adf7446c823418e1af54cb
hgsgtk <hgsgtk@gmail.com>
2021-08-28 09:22:41 +0900

    feat: add configuration parser
    compile error, but commietted

--- httptunnel/copying/src/configuration.rs
+++ httptunnel/copying/src/configuration.rs
@@ -1,5 +1,7 @@
 use crate::relay::{RelayPolicy};
 
+use clap::clap_app;
+use log::info;
 // rust-native-tls: An abstraction over platform-specific TLS implementations.
 // https://crates.io/crates/native-tls
 use native_tls::Identity;
@@ -96,17 +98,173 @@ pub struct ProxyConfiguration {
 // https://doc.rust-lang.org/std/keyword.impl.html
 impl ProxyConfiguration {
     pub fn from_command_line() -> io::Result<ProxyConfiguration> {
-        // TODO: command line
+        // Crate clap: clap is a simple-to-use, efficient, and full-featured library for parsing command line arguments and subcommands when writing console/terminal applications.
+        // https://docs.rs/clap/2.22.2/clap/index.html
+        // clap_app! macro https://github.com/clap-rs/clap/issues/1347
+        /**
+-> % ./target/debug/copying --help
+Copied simple HTTP(S) Tunnel 0.0.1
+
+Kazuki Higashiguchi
+
+A simple HTTP(S) tunnel
+
+USAGE:
+    copying [OPTIONS] --bind <BIND> [SUBCOMMAND]
+
+FLAGS:
+    -h, --help       Print help information
+    -V, --version    Print version information
+
+OPTIONS:
+        --bind <BIND>        Bind address, e.g. 0.0.0.0:8443
+        --config <CONFIG>    Configuration file
+
+SUBCOMMANDS:
+    help     Print this message or the help of the given subcommand(s)
+    http     Run the tunnel in HTTP mode
+    https    Run the tunnel in HTTPS mode
+    tcp      Run the tunnel in TCP proxy mode
+        */
+        let matches = clap_app!(myapp => 
+            (name: "Copied simple HTTP(S) Tunnel")
+            (version: "0.0.1")
+            (author: "Kazuki Higashiguchi")
+            (about: "A simple HTTP(S) tunnel")
+            // @: Pattern binding
+            // https://doc.rust-lang.org/book/appendix-02-operators.html
+            (@arg CONFIG: --config +takes_value "Configuration file")
+            (@arg BIND: --bind +required +takes_value "Bind address, e.g. 0.0.0.0:8443")
+            (@subcommand http =>
+                (about: "Run the tunnel in HTTP mode")
+                (version: "0.0.1")
+            )
+            (@subcommand https =>
+                (about: "Run the tunnel in HTTPS mode")
+                (version: "0.0.1")
+                // PKCS12 https://en.wikipedia.org/wiki/PKCS_12
+                // > PKCS #12 defines an archive file format for storing many cryptography objects as a single file
+                // https://qiita.com/kunichiko/items/3e2ec27928a95630a73a
+                (@arg PKCS12: --pk +required +takes_value "pkcs12 filename")
+                (@arg PASSWORD: --password +required +takes_value "Password for the pkcs12 file")
+            )
+            (@subcommand tcp => 
+                (about: "Run the tunnel in TCP proxy mode")
+                (version: "0.0.1")
+                (@arg DESTINATION: --destination -d +required +takes_value "Destination address, e.g. 10.0.0.2:8443")
+            )
+        )
+        .get_matches();
+
+        // We can get value by API value_of()
+        // https://docs.rs/clap/2.33.3/clap/struct.ArgMatches.html
+        let config = matches.value_of("CONFIG");
+
+        let bind_address = matches
+            .value_of("BIND")
+            .expect("misconfiguration for bind")
+            .to_string();
+
+        // subcommand_matches()
+        // > This method returns the ArgMatches for a particular subcommand or None if the subcommand wasn't present at runtime.
+        // https://docs.rs/clap/2.33.3/clap/struct.ArgMatches.html#method.subcommand_matches
+        // Argmatches is Option type, which has is_some().
+        // > Returns true if the option is a Some value.
+        // https://doc.rust-lang.org/beta/core/option/enum.Option.html
+        let mode = if matches.subcommand_matches("http").is_some() {
+            // If user has a subcommand that uses http mode
+            // Crate info!
+            // https://qiita.com/fujitayy/items/590145c0f4b4e7d06de7
+            info!(
+                "Starting in HTTP mode: bind: {}, configuration: {:?}",
+                bind_address, config
+            );
+            ProxyMode::HTTP
+        } else if let Some(https) = matches.subcommand_matches("https") {
+            let pkcs12_file = https
+                .value_of("PKCS12")
+                .expect("misconfiguration for pkcs12");
+            let password = https
+                .value_of("PASSWORD")
+                .expect("misconfiguration for password");
+            
+            // ?: Error propagation
+            // https://doc.rust-lang.org/book/appendix-02-operators.html
+            let identity = ProxyConfiguration::tls_identify_from_file(pkcs12_file, password)?;
+            info!(
+                "Starting in HTTPS mode: pkcs12: {}, password: {}, bind: {}, configuration: {:?}",
+                pkcs12_file,
+                // !: Bitwise or logical complement (in short, "NOT")
+                !password.is_empty(),
+                bind_address,
+                config
+            );
+            ProxyMode::HTTPS(identity)
+        } else if let Some(tcp) = matches.subcommand_matches("tcp") {
+            let destination = tcp
+                .value_of("DESTINATION")
+                .expect("misconfiguration for destination")
+                .to_string();
+            info!(
+                "Starting in TCP mode: destination: {}, configuration: {:?}",
+                destination, config
+            );
+            ProxyMode::TCP(destination)
+        } else {
+            // Indicates unreachable code.
+            // This will always panic!.
+            // https://doc.rust-lang.org/std/macro.unreachable.html
+            // I send nits improvement PR :) https://github.com/xnuter/http-tunnel/pull/8 .
+            unreachable!("only http, https and tcp commands are supported")
+        }
+
+        // TODO: create tunnel default config
+        // Fixme: fake it!
+        let tunnel_config = TunnelConfig {
+            client_connection: ClientConnectionConfig {
+                // ex. Duration::new(10, 0) means 10 seconds
+                // https://doc.rust-lang.org/stable/std/time/struct.Duration.html
+                initiation_timeout: Duration::new(10, 0)
+                relay_policy: RelayPolicy {
+                    idle_timeout: Duration::new(30, 0),
+                    min_rate_bpm: 1000,
+                    max_rate_bpm: 10000,
+                }
+            },
+            target_connection: TargetConnectionConfig {
+                dns_cache_ttl: Duration::new(60, 0),
+                allowed_targets: "^(?i)([a-z]+)\\.(wikipedia|rust-lang)\\.org:443$",
+                connect_timeout: Duration::new(10, 0)
+                relay_policy: RelayPolicy {
+                    idle_timeout: Duration::new(30, 0),
+                    min_rate_bpm: 1000,
+                    max_rate_bpm: 10000,
+                }
+            }
+        }
         
         // derive_builder allows us to build structs Builder pattern.
         // https://docs.rs/derive_builder/0.10.2/derive_builder/#builder-patterns
         // Without calling build(), we got the following error.
         // > expected struct `ProxyConfiguration`, found struct `ProxyConfigurationBuilder`
         Ok(ProxyConfigurationBuilder::default()
-            // TODO set some values
-            // Now we got an error at runtime.
+            .bind_address(bind_address)
+            .mode(mode)
+            .tunnel_config(tunnel_config)
+            // Withoug any binding, we got an error at runtime.
             // > thread 'main' panicked at 'ProxyConfigurationBuilder failed: "`mode` must be initialized"', src/configuration.rs:108:14
             .build()
             .expect("ProxyConfigurationBuilder failed"))
+    }
+
+
+    fn tls_identify_from_file(filename: &str, password: &str) -> io::Result<Identity> {
+        // TODO: get identiry from file
+
+        // Fixme: fake it
+        // Instantiate struct
+        // https://doc.rust-jp.rs/book-ja/ch05-01-defining-structs.html
+        // TODO: initialize Identity
+        Ok(result)
     }
 }
